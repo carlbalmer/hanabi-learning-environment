4e50021c046e23c69795749b98ef919168b8c2dd
Carl Balmer <carl@brogrammers.ch>
2019-09-24 10:31:47 +0200

    added install targets and setup.py/project.toml config

---
+++ setup.py
@@ -0,0 +1,9 @@
+from skbuild import setup
+
+setup(
+    name='hanabi-learning-environment',
+    version='0.0.1',
+    description='Reinforcement learning environment for the game of hanabi.',
+    author='Forked from Deepmind',
+    packages=['hanabi_learning_environment']
+)
